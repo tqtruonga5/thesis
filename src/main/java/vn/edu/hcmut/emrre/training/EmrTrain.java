a39aa85f6136e172c94bbc80dbb46705af945ec3
Pham Van Lap <lappham93@gmail.com>
2015-09-14 14:48:04 +0700

    add check 2 concepts relational

--- src/main/java/vn/edu/hcmut/emrre/training/EmrTrain.java
+++ src/main/java/vn/edu/hcmut/emrre/training/EmrTrain.java
@@ -11,26 +11,29 @@ import vn.edu.hcmut.emrre.core.entity.Relation;
 import vn.edu.hcmut.emrre.core.io.DataReader;
 
 public class EmrTrain {
-	private int numOfTraining;
-	
-	public void training(List<DocLine> docLines, List<Concept> concepts, List<Relation> relations){
-		// duyet tat ca cac file
-    	// 1 file  
+    private int numOfTraining;
 
-        //chua tat ca data training
+    public void training(List<DocLine> docLines, List<Concept> concepts, List<Relation> relations) {
+        // duyet tat ca cac file
+        // 1 file
+
+        // chua tat ca data training
         List<Double[][]> trainingData;
         System.out.println(relations.size());
-        long timeStart = System.nanoTime();       
-        for (int i = 0; i < concepts.size() - 1 ; i ++)
-        	for (int j = i + 1; j < concepts.size(); j++){
-        		//check if there doesn't exist relation between 2 concepts
-        		//add a relation NONE into relations list
-        		if (!Relation.hasRelation(concepts.get(i), concepts.get(j), DocLine.getDocLine(docLines, concepts.get(i).getLine()))){
-        			relations.add(new Relation(concepts.get(i).getKey(), concepts.get(j).getKey(), Relation.Type.NONE, relations.size()));
-        		}
-        	}
+        long timeStart = System.nanoTime();
+        for (int i = 0; i < concepts.size() - 1; i++)
+            for (int j = i + 1; j < concepts.size(); j++) {
+                // check if there doesn't exist relation between 2 concepts
+                // add a relation NONE into relations list
+                if (Relation.canRelate(concepts.get(i), concepts.get(j)))
+                    if (!Relation.hasRelation(concepts.get(i), concepts.get(j),
+                            DocLine.getDocLine(docLines, concepts.get(i).getLine()))) {
+                        relations.add(new Relation(concepts.get(i).getKey(), concepts.get(j).getKey(),
+                                Relation.Type.NONE, relations.size()));
+                    }
+            }
         long time = System.nanoTime() - timeStart;
         System.out.println(relations.size());
         System.out.println("Time run: " + time);
-	}
+    }
 }
